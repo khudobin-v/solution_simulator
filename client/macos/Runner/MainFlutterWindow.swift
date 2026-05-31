22bbcb91eb5202761f7454c92f1f170b2d994b47
Vasiliy Khudobin <khudobin_v@vk.com>
2026-05-31 20:29:24 +0300

    Запуск в полноэкранном режиме по умолчанию

---
+++ client/macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,20 @@
+import Cocoa
+import FlutterMacOS
+
+class MainFlutterWindow: NSWindow {
+  override func awakeFromNib() {
+    let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    super.awakeFromNib()
+
+    // Launch in full screen by default
+    DispatchQueue.main.async {
+      self.toggleFullScreen(nil)
+    }
+  }
+}
